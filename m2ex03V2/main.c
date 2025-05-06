9455254078a30b592db0091bdf38b19685c63373
Arnon_Noteboook <arnonjra@gmail.com>
2025-05-06 09:42:05 -0300

    add ex03V2

--- m2ex03V2/main.c
+++ m2ex03V2/main.c
@@ -2,10 +2,11 @@
 Refaça o exercício 2 removendo os rebotes das chaves. Para isso, defina uma função
 debounce() que consome tempo do processador através de um loop que decremente uma
 variável. Não deixe de declará-la como volatile para evitar que o recurso de otimização do
-compilador a remova.*/
+compilador a remova. SEM OCUPAR O PROCESSADOR*/
 
 #include <msp430.h>
 #include "system_init.h"
+#include <stdbool.h>
 
 void io_config(void);
 void debounce(volatile unsigned int i);
@@ -14,23 +15,29 @@ int main(void)
 {
   volatile unsigned int i;
   WDTCTL = WDTPW+WDTHOLD;               // Stop WDT
+  
+  bool estado_anterior = P1IN & BIT1; //chave aberta
   io_config();
   while(1)                                
   {
-    while (P1IN & BIT1) { 
+    //estado_anterior = P1IN & BIT1;
+    if ((P1IN & BIT1) & estado_anterior) {
+      estado_anterior = 1;
+    }else if ((P1IN & BIT1) & !estado_anterior) { 
+      estado_anterior = 1;
+      debounce(50000);
     }
-    P1OUT ^= BIT0;
-    debounce(50000);
-    while (!(P1IN & BIT1)){
+
+    if (!(P1IN & BIT1) & !estado_anterior) {
+      estado_anterior = 0;  //chave aberta
+    }else if (!(P1IN & BIT1) & estado_anterior) {
+      estado_anterior = 0;  //chave fechada
+      P1OUT ^= BIT0; 
+      debounce(50000);
     }
-    debounce(50000);
   }  
 }
 
-
 void debounce(volatile unsigned int i){
-
-  //volatile unsigned int cnt = i;
   while (i--){}
-
 }
---
+++ m2ex03V2/system_init.h
@@ -0,0 +1,12 @@
+
+void io_config(void){
+  P1DIR = P1DIR | BIT0;
+  P1OUT = P1OUT & ~BIT0;                     // P1.0 set as low logic signal
+  P1REN = P1REN & ~BIT0;                    //Preciso dessa linha? não pois P1.0 está setado como saída(ver datasheet)
+  P1SEL = P1SEL & ~BIT0;
+                   
+  P1DIR &= ~BIT1;
+  P1OUT |= BIT1;                
+  P1REN |= BIT1;               
+  P1SEL &= ~BIT1;
+}

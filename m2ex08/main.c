22ba302be9729c6254e52eb4082f250b64fd1b8d
Arnon_Noteboook <arnonjra@gmail.com>
2025-05-15 21:30:19 -0300

    ex08 correction interruption and CCR0, CCR1 values

--- m2ex08/main.c
+++ m2ex08/main.c
@@ -11,20 +11,32 @@ void main(void)
     WDTCTL = WDTPW | WDTHOLD;
     //set timer A0
     TA0CTL = MC__UP | TASSEL__ACLK;
-    TA0CCR0 = 255;
-    TA0CCR1 = 127;
-
+    TA0CCTL0 = CCIE;
+    TA0CCTL1 = CCIE;
+    TA0CCR0 = 256;
+    TA0CCR1 = 128;
+    //set P1.0 (LED 1 vermelho)
     P1DIR |= BIT0;
 
+    __enable_interrupt();
     while (1) 
     {
-        if (TA0CCTL1 & CCIFG) 
-        {
-            P1OUT ^= BIT0;
-            TA0CCTL1 &= ~CCIFG;   
-        }else if (TA0CTL & TAIFG) {
-            P1OUT ^= BIT0;
-            TA0CTL &= ~BIT0;
-        }
+    }
+}
+
+#pragma vector = 53
+__interrupt void isr_ta0_ccr0()
+{
+    P1OUT &= ~BIT0;
+}
+
+#pragma vector = 52
+__interrupt void isr_ta0_ccr1()
+{
+    switch (TA0IV){
+        case 0x00: break;
+        case 0x02:
+            P1OUT |= BIT0;
+        default: break;
     }
 }
